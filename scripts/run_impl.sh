6a7cdcd4f403b7c28d4eeb10026b29ec7fb90fba
Giovanni Silva <giovanni@atende.info>
2015-03-14 19:22:21 -0300

    bamboo version 5.7.2

--- scripts/run_impl.sh
+++ scripts/run_impl.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+start() {
+  # Ensure the volume home has the correct permissions, because this can cause errors
+  chown ${SOFTWARE_NAME}.${SOFTWARE_NAME} -R /opt/${SOFTWARE_NAME}-home
+  exec /sbin/setuser ${SOFTWARE_NAME} /bin/bash /opt/${SOFTWARE_NAME}/bin/start-${SOFTWARE_NAME}.sh
+}
